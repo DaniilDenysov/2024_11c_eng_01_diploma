e5940bcebc41deebcaab64aa15bfa49e35c24b39
Daniil <den.danya.denysov@gmail.com>
2024-12-02 19:33:34 +0100

    Changed recoil recovery order

--- Assets/Scripts/PlayerCameraController.cs
+++ Assets/Scripts/PlayerCameraController.cs
@@ -59,10 +59,10 @@ public class PlayerCameraController : NetworkBehaviour
             return;
         }
 
+        RecoverRecoil();
         HandleMouseLook();
         HandleFieldOfView();
        // HandleHeadBobbing();
-        RecoverRecoil();
     }
 
     void HandleMouseLook()
